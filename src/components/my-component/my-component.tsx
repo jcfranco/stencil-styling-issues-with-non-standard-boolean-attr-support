dbe1247d6292d04229c676efb208356c655e395d
JC Franco <jfranco@esri.com>
2024-01-05 10:50:00 -0800

    chore: set up repro case

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,4 @@
 import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
 
 @Component({
   tag: 'my-component',
@@ -7,26 +6,9 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
-
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
+  @Prop({ reflect: true }) on: boolean = false;
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>{this.on ? "🔛" : "📴"}<slot></slot></div>;
   }
 }
